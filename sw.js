e63e1b26bf3eebe669d6264b07a62a9497e76524
devetest <jgdev2019@gmail.com>
2019-09-06 09:30:45 -0500

    Primer commit

---
+++ sw.js
@@ -0,0 +1,77 @@
+// Imports
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.10.2/css/all.css',
+    'css/animate.css',
+    'js/libs/jquery.js'
+];
+
+self.addEventListener('install', e => {
+
+    // Abrir el cache statico y agregar el arreglo con el contenido estatico
+    const cacheStatic = caches.open(STATIC_CACHE).then(cache => cache.addAll(APP_SHELL));
+
+    // Abrir el cache inmutable y agregar el arreglo con el contenido inmutable
+    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => cache.addAll(APP_SHELL_INMUTABLE));
+
+    // Esperar hasta que se resuelvan las promesas del cache estatico e inmutable,
+    // antes de salir del evento de instalacion
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+});
+
+self.addEventListener('activate', e => {
+
+    const respuesta = caches.keys().then(keys => {
+
+        keys.forEach(key => {
+
+            if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+        });
+    });
+
+    e.waitUntil(respuesta);
+});
+
+self.addEventListener('fetch', e => {
+
+    // Estrategia: Cache with Network Fallback
+    const respuesta = caches.match(e.request).then(res => {
+
+            if (res) {
+                return res;
+            } else {
+                // console.log(e.request.url);
+                return fetch(e.request).then(newRes => {
+
+                    return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+                });
+            }
+
+        })
+        .catch(error => console.log(error, 'cual'));
+
+    e.respondWith(respuesta);
+});
